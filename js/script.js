e0d1bbc9930c3b08cf531cc65ca542b31e579d2a
MarkusTech <55816864+MarkusTech@users.noreply.github.com>
2022-08-03 11:29:53 +0800

    new update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ","Web Developer","Backend Developer","Full-stack Developer","Sytem Developer"],
+    strings:[" ","Web Developer","Backend Developer","Full-stack Developer","System Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
